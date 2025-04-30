c5b715ecefe17641efb1fce00803e355c05772a8
gedar1 <gedar1971@gmail.com>
2025-04-29 20:45:13 -0500

    create project

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="proyecto_integrado_V",
+    version="0.0.1",
+    author="German Arbelaez & Juan Carlos Presiaga",
+    author_email="",
+    description="",
+    py_modules=["Linea de Énfasis (Entrega 1)"],
+    install_requires=[
+        "pandas",
+        "openpyxl",
+        "requests",
+        "python-dotenv"
+    ] 
+)
