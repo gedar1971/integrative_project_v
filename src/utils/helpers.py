e85ffb3f71d6cf133bc0a13881cde7a76c194bac
gedar1 <gedar1971@gmail.com>
2025-05-09 19:53:24 -0500

    feat: :sparkles: create file helpers

---
+++ src/utils/helpers.py
@@ -0,0 +1,48 @@
+import json
+import os
+import io
+import pandas as pd
+from datetime import datetime
+
+
+def create_file(data, filename, file_format='json'):
+    os.makedirs(os.path.dirname(filename), exist_ok=True)
+
+    if file_format == 'json':
+        with open(filename, 'w') as json_file:
+            json.dump(data, json_file, indent=4)
+        print(f"Archivo JSON '{filename}' generado exitosamente.")
+
+    elif file_format == 'xlsx':
+        if isinstance(data, pd.DataFrame):
+            data.to_excel(filename, index=False)
+            print(f"Archivo Excel '{filename}' generado exitosamente.")
+        else:
+            raise ValueError("Los datos deben ser un DataFrame para guardar como Excel.")
+
+    elif file_format == 'csv':
+        if isinstance(data, pd.DataFrame):
+            data.to_csv(filename, index=False)
+            print(f"Archivo CSV '{filename}' generado exitosamente.")
+        else:
+            raise ValueError("Los datos deben ser un DataFrame para guardar como CSV.")
+    elif file_format == 'txt':
+        if isinstance(data, pd.DataFrame):
+            data.to_txt(filename, index=False)
+            print(f"Archivo TXT '{filename}' generado exitosamente.")
+        else:
+            raise ValueError("Los datos deben ser un DataFrame para guardar como TXT.")
+
+    else:
+        raise ValueError(f"Formato de archivo no soportado: {file_format}")
+    
+
+def convert_to_numeric(df, numeric_columns):
+    for column in numeric_columns:
+        df[column] = pd.to_numeric(df[column], errors='coerce')
+    return df
+
+def convert_to_lowercase(df, text_lowercase_columns):
+    for column in text_lowercase_columns:
+        df[column] = df[column].str.lower()
+    return df
