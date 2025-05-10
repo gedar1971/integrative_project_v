781a2ff9be1c6ceae34127e15a0c6723c8440262
jcpresiga <jcpresiga@gmail.com>
2025-05-09 19:51:14 -0500

    create file logger.py

--- src/logger.py
+++ src/logger.py
@@ -1,1 +1,31 @@
-print('Logger Actividad')
+import logging
+import os
+from src.utils.helpers import create_file
+
+def setup_logger():
+
+    log_dir = os.path.join('docs', 'logs')
+    os.makedirs(log_dir, exist_ok=True)
+
+    log_file = os.path.join(log_dir, 'application.txt')
+
+    logger = logging.getLogger('DataCollector')
+    logger.setLevel(logging.INFO)
+
+    if not logger.handlers:
+        file_handler = logging.FileHandler(log_file)
+        file_handler.setLevel(logging.INFO)
+        
+        console_handler = logging.StreamHandler()
+        console_handler.setLevel(logging.INFO)
+        
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        file_handler.setFormatter(formatter)
+        console_handler.setFormatter(formatter)
+        
+        logger.addHandler(file_handler)
+        logger.addHandler(console_handler)
+
+    return logger
+
+logger = setup_logger()
